0b076170e6109aebb31f5cb48aa1bca0aa9fff06
gullr <rehman.gull381@gmail.com>
2019-12-28 18:29:47 +0500

    bug-fix

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# NgNew
+# AngularDeployTest
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.2.5.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.3.21.
 
 ## Development server
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {FormsModule} from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HajajComponent } from './hajaj/hajaj.component';
-import { HajajDetailComponent } from './hajaj-detail/hajaj-detail.component';
-import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HajajComponent,
-    HajajDetailComponent,
-    MessagesComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
